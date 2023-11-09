2491d368760bbf7230229f3132beb659169752b0
madvorak <martin.dvorak@matfyz.cz>
2023-11-09 18:50:37 +0100

    notes slightly improved

--- Fecssk/Class06.lean
+++ Fecssk/Class06.lean
@@ -29,23 +29,24 @@ Rule `R` is sound iff [if all premises of `R` are valid, then the conclusion of 
 Formal system `F` is sound iff all rules are sound (or equivalently, every theorem is valid).
 Formal system `F` is complete iff every valid formula is a theorem.
 Formal system `F` is consistent unless `⊢ ⊥` (or equivalently, there exists a formula that is not a theorem).
-Rule `R` is derivable in `F` iff [for all formulas `φ`, `F ∪ {R}`-under-turnstile `φ` iff  `F`-under-turnstile `φ`].
+Rule `R` is derivable in `F` iff [∀ formulas `φ`, `F ∪ {R}`-under-turnstile `φ` iff  `F`-under-turnstile `φ`].
 Rule `R` is admissible in `F` iff `F ∪ {R}` is still consistent.
-Formula `φ` is expressible in a logic `L` iff
-[∃ formula `ψ` of `L` such that ∀ interpretations `v` we have `⁅φ⁆ᵥ = ⁅ψ⁆ᵥ`].
+Formula `φ` is expressible in a logic `L` iff [∃ formula `ψ` of `L` such that ∀ interpretations `v`
+  `⟦φ⟧ᵥ = ⟦ψ⟧ᵥ`].
 For example `φ₁ ∧ φ₂` is expressible using only `¬` and `∨` (de Morgan) as `ψ = ¬ (¬φ₁ ∨ ¬φ₂)`.
 
 We can enumerate all theorems by systematically enumerating all possible proofs.
-The witness for validity is a proof.
+The proof is a witness for validity.
 Sound formal system gives a sound procedure for validity (but not necessarily complete).
-Sound complete formal system gives a sound semi-complete procedure for validity (may not terminate on invalid inputs).
+Sound complete formal system gives a sound semi-complete procedure for validity (may not terminate on inputs
+that represent a formula that is not valid).
 To get a decision procedure (sound and complete procedure for validity), we need both
 (1) sound complete formal system for validity, and
 (2) sound complete formal system for satisfiability (to define a formal system for satisfiability,
     replace "formulas" (`φ` is valid) by "judgements" (`φ` is satisfiable);
     all axioms are satisfiable, all rules go from satisfiables to satisfiable).
 For every input `φ`, one of them will eventually terminate.
-Conclude that `φ` is valid or `¬φ` is satisfiable.
+Conclude; either `φ` is valid, or `¬φ` is satisfiable (which means that `φ` is not valid).
 If both a set and its complement are recursively-enumerable, the set is recursive (decidable).
 
 Example (formal system for unsatisfiability):
@@ -74,7 +75,7 @@ Axioms:
 Metatheorem: Hilbert system is sound and complete for propositional logic.
 Corollary: Hilbert system is consistent.
 
-Example (prove `φ → φ`):
+Example (prove `φ → φ` in Hilbert system):
 
 (K) `φ → (ψ → φ) → φ`
 (S) `(φ → (ψ → φ) → φ) → ((φ → ψ → φ) → (φ → φ))`
@@ -94,6 +95,8 @@ Induction on length `n` of `π`.
 Case `n=1`: `ψ` must be an axiom. Case analysis. TODO.
 Case `n>1`: `ψ` is the application of modus ponens. TODO.
 
-Homework: `(φ → ψ) → (ψ → χ) → (φ → χ)` in Hilbert system
+#### Homework
+
+Prove `(φ → ψ) → (ψ → χ) → (φ → χ)` in Hilbert system.
 
 -/
